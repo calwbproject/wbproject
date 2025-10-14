642ecf31ff0ef3d5e9a2a7bc1eeb5fafc4eca11b
KYEek <dltnstls89@hotmail.com>
2025-10-14 17:49:42 +0900

    view_setting

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Question, Answer
 
-# Register your models here.
-admin.site.register(Question)
+# Register your models here.)
 admin.site.register(Answer)
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields=['subject']
+    
+admin.site.register(Question,QuestionAdmin)
